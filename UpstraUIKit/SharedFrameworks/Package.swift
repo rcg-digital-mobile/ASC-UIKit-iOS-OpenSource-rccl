56e730bb40e8191d1be03c03dbe84a0ee6e919f7
Ekaterina Gumnova <egumnova@rccl.com>
2025-09-19 15:43:57 -0400

    Updated Realm to 10.50

--- UpstraUIKit/SharedFrameworks/Package.swift
+++ UpstraUIKit/SharedFrameworks/Package.swift
@@ -27,15 +27,15 @@ let package = Package(
                     checksum: "31e2bb135560e3c327017e5378446535297a035016709a359765d2d2ea10cd17"
                 ),
         .binaryTarget(
-                    name: "Realm",
-                    url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/4.12.0/Realm.xcframework.zip",
-                    checksum: "b7ab411535032a85dca7b805d65321c7dfea52931aed85fc8d1965a4eee4d61b"
-                ),
-         .binaryTarget(
-                    name: "RealmSwift",
-                    url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/4.12.0/RealmSwift.xcframework.zip",
-                    checksum: "997c7500bc0280ca790c98baae8ab5576b9fb082668b7ed94371ffb818c5daa4"
-                ),
+            name: "Realm",
+            url: "https://sdk.amity.co/sdk-release/realm-frameworks/10.50.0/Realm.xcframework.zip",
+            checksum: "67a140737ba2fd92e0da9b692ab6dd6b8d7ede90bd4292152e9d6d19f70b6d38"
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "https://sdk.amity.co/sdk-release/realm-frameworks/10.50.0/RealmSwift.xcframework.zip",
+            checksum: "1f6c3d9580a811afbc04a3a2726327719c76d988224ddb694e7eefaf49e31b10"
+        ),
         .binaryTarget(
                     name: "AmityLiveVideoBroadcastKit",
                     url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/4.12.0/AmityLiveVideoBroadcastKit.xcframework.zip",
